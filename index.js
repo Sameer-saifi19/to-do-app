91f7592c6c89e3aff90604dfffbd3334abd996b6
Sameer <sameer.edu19@gmail.com>
2025-06-28 10:59:21 +0530

    Added React

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const cors = require('cors')
 const todoModel = require('./db');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
-app.use(express.json)
+app.use(express.json())
 app.use(cors())
 
 app.post('/create-task', async function (req, res) {
 
     const { title, description, date, done } = req.body;
-
     try {
         await todoModel.create({
-            title,
-            description,
-            date,
-            done
+            title: title,
+            description: description,
+            date: date,
+            done: done
+        })
+
+        res.json({
+            message: "task created"
         })
     } catch (error) {
         console.log("Error !...Cannot create task", Error)
@@ -25,4 +30,31 @@ app.post('/create-task', async function (req, res) {
 
 })
 
-app.listen(3000)
+
+app.get('/get-todos',async (req, res) =>{
+    try {
+        const allTodos = await todoModel.find()
+
+        if(allTodos.length === 0){
+            res.json({
+                Message: "no task found"
+            })
+        }
+        
+        res.status(201).json({
+            allTodos
+        })
+    } catch (error) {
+        res.json({
+            message: "error fetching docs"
+        })
+    }
+})
+
+async function connection(){
+    mongoose.connect("mongodb+srv://user1:CSA123@cluster0.e4e43lo.mongodb.net/todo-app")
+    app.listen(3000)
+    console.log("app is listening at 3000")
+}
+
+connection()
